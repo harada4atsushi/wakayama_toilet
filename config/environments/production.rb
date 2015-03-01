69ec06834baa95de92d5a5b5ee4506c9f6685e23
川村亮輔 <ryosuke@kawamurukenoair.lan>
2015-03-01 18:09:27 +0900

    asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -57,6 +57,8 @@ Rails.application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets.
+  config.assets.precompile += %w(*.js)
+  config.assets.precompile += %w(*.css)
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
